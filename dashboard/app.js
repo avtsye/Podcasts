dae423c298ed9e125eed957f7c8d026125f38ad7
אברהם mch <avtsye@gmail.com>
2026-09-24 14:57:25 +0300

    Configure dashboard API endpoint

--- dashboard/app.js
+++ dashboard/app.js
@@ -1,4 +1,4 @@
-const API="__API_URL__";
+const API="https://podcasts-dashboard-api.onrender.com";
 let password=sessionStorage.getItem("podcastDashboardPassword")||"";
 let data=null;
 const $=s=>document.querySelector(s), view=$("#view");
